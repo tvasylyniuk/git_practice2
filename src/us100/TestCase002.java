30cc5aae8a584cbce0bb577b2bd08066d2584ed3
Tatiana Vasylyniuk <tatianavasylyniuk@gmail.com>
2022-01-11 12:18:29 -0500

    second description created

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -5,5 +5,7 @@ public class TestCase002 {
         System.out.println("Hello Again");
 
         System.out.println("We are learning Git");
+
+        System.out.println("I love java and GIT");
     }
 }
